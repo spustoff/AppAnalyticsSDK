d536730b3612b94da93e6d45696ee09fe01c27cc
spustoff <vyacheslavpustov05@gmail.com>
2025-02-09 15:59:31 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppAnalytics",
-            url: "https://github.com/spustoff/AppAnalytics/releases/download/1.0.0/AppAnalytics.xcframework.zip",
+            url: "https://github.com/spustoff/AppAnalyticsSDK/releases/download/1.0.0/AppAnalytics.xcframework.zip",
             checksum: "f8273d154a4080a5023d7a653b275fbca5b1bb8417016fe18aa623efe6310ffd"
         )
     ]
