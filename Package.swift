950dc9ff79ff4cf8a039c9c7742b6fbcb5f33506
spustoff <vyacheslavpustov05@gmail.com>
2025-02-09 15:57:20 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AppAnalyticsSDK",
+    platforms: [
+        .iOS(.v14) // Минимальная версия iOS 14+
+    ],
+    products: [
+        .library(
+            name: "AppAnalytics",
+            targets: ["AppAnalytics"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AppAnalytics",
+            url: "https://github.com/spustoff/AppAnalytics/releases/download/1.0.0/AppAnalytics.xcframework.zip",
+            checksum: "f8273d154a4080a5023d7a653b275fbca5b1bb8417016fe18aa623efe6310ffd"
+        )
+    ]
+)
